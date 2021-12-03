d9f408ac03277fa5980707b4962c544d27864216
JesseKartabani <jessekartabani@gmail.com>
2021-12-03 23:35:51 +1100

    Fixed wording on a comment

--- encryptionDecryption.py
+++ encryptionDecryption.py
@@ -27,7 +27,7 @@ label1 = ttk.Label(root, text="Enter secret key for encryption and decryption")
 label1.config(font=("calibri",12))
 label1.place(x=10, y=200)
 
-# widget allows user to input text
+# widget allows user to input their text
 user_text = tk.Entry (root)
 user_text.config()
 user_text.pack(pady=10)
