0b98bcb6ad26d9a02ae8deb40276afd4873ed2f5
JesseKartabani <jessekartabani@gmail.com>
2021-12-03 22:55:00 +1100

    Added basic widget that allows user to input text

--- encryptionDecryption.py
+++ encryptionDecryption.py
@@ -17,8 +17,13 @@ root.geometry("400x400")
 # set window title
 root.title("Title Here")
 
+# static label
 label = ttk.Label(root, padding=10, text="Enter text for encryption and decryption")
 label.config(font=("calbri",13), foreground="black")
 label.pack()
+
+# widget allows user to input text
+user_entry = tk.Entry (root)
+user_entry.pack(pady=10)
 
 root.mainloop()
