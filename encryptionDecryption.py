7e2c0663156bf409a3a52a74318a8779d33842bd
JesseKartabani <jessekartabani@gmail.com>
2021-12-08 06:31:49 +1100

    Changed code styling according to flake8

--- encryptionDecryption.py
+++ encryptionDecryption.py
@@ -1,6 +1,6 @@
 import base64
 import tkinter as tk
-from tkinter import Button, Label, PhotoImage, Text, Toplevel, ttk, font, messagebox
+from tkinter import Button, Label, Text, Toplevel, ttk, messagebox
 from tkinter.constants import END, GROOVE, WORD
 
 # -- Windows only configuration --
@@ -11,21 +11,25 @@ except:
     pass
 # -- End Windows only configuration --
 
+
 def decrypt():
-    """ Decrypts user inputed text using base64 and displays it in a new window"""
+    """
+    Decrypts user inputed text using base64
+    and displays it in a new window
+    """
 
     password = user_pass.get()
     # checks password is correct
     if password == "1234":
-        root2 = Toplevel(root)        # creates new window
-        root2.title("Decryption")     # sets window title
-        root2.geometry("400x200")     # sets window size
-        root2.configure(bg="#00bd56") # sets window background color
+        root2 = Toplevel(root)         # creates new window
+        root2.title("Decryption")      # sets window title
+        root2.geometry("400x200")      # sets window size
+        root2.configure(bg="#00bd56")  # sets window background color
 
-        message = user_text.get(1.0, "end") # stores user input
+        message = user_text.get(1.0, "end")  # stores user input
 
         # Decrypts message
-        decode_message = message.encode("ascii") 
+        decode_message = message.encode("ascii")
         base64_bytes = base64.b64decode(decode_message)
         decrypt = base64_bytes.decode("ascii")
 
@@ -34,7 +38,7 @@ def decrypt():
 
         # displays empty text box
         text1 = Text(root2, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
-        text1.place(x=10, y=40, width= 380, height=150)
+        text1.place(x=10, y=40, width=380, height=150)
 
         # displays the actual decrytped text
         text1.insert(END, decrypt)
@@ -45,21 +49,25 @@ def decrypt():
     else:
         messagebox.showerror("Encryption", "Invalid Password")
 
+
 def encrypt():
-    """ Encrypts user inputed text using base64 and displays it in a new window """
+    """
+    Encrypts user inputed text using base64
+    and displays it in a new window
+    """
 
     password = user_pass.get()
     # checks password is correct
     if password == "1234":
-        root1 = Toplevel(root)        # creates new window
-        root1.title("Encryption")     # sets window title
-        root1.geometry("400x200")     # sets window size
-        root1.configure(bg="#ed3833") # sets window background color
+        root1 = Toplevel(root)         # creates new window
+        root1.title("Encryption")      # sets window title
+        root1.geometry("400x200")      # sets window size
+        root1.configure(bg="#ed3833")  # sets window background color
 
-        message = user_text.get(1.0, "end") # stores user input
+        message = user_text.get(1.0, "end")  # stores user input
 
         # encodes message
-        encode_message = message.encode("ascii") 
+        encode_message = message.encode("ascii")
         base64_bytes = base64.b64encode(encode_message)
         encrypt = base64_bytes.decode("ascii")
 
@@ -68,7 +76,7 @@ def encrypt():
 
         # displays empty text box
         text1 = Text(root1, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
-        text1.place(x=10, y=40, width= 380, height=150)
+        text1.place(x=10, y=40, width=380, height=150)
 
         # displays the actual encrytped text
         text1.insert(END, encrypt)
@@ -78,8 +86,7 @@ def encrypt():
     # displays error message when password is incorrect
     else:
         messagebox.showerror("Encryption", "Invalid Password")
-        
-    
+
 
 def reset():
     """ Removes all user inputed text when reset button is clicked """
@@ -87,12 +94,13 @@ def reset():
     user_text.delete("1.0", "end")
     user_pass.delete("0", "end")
 
+
 def copy():
     """ Copys inputed text to clipboard when copy button is clicked """
-    ### TODO
+    # TODO
+
 
 # MAIN SCREEN
-
 root = tk.Tk()
 # set window size
 root.geometry("400x400")
@@ -105,21 +113,21 @@ root.title("Ciphertext")
 
 # static label that tells user to input text
 label = ttk.Label(root, padding=10, text="Enter text for encryption and decryption")
-label.config(font=("calbri",13), foreground="black",)
+label.config(font=("calbri", 13), foreground="black")
 label.pack()
 
 # static label that asks for users password
 label1 = ttk.Label(root, text="Enter secret key for encryption and decryption")
-label1.config(font=("calibri",12))
+label1.config(font=("calibri", 12))
 label1.place(x=10, y=200)
 
 # widget allows user to input their password
-user_pass = tk.Entry (root)
+user_pass = tk.Entry(root)
 user_pass.config(font="Robote 20", background="white", relief=GROOVE, bd=0, show="*")
 user_pass.place(x=28, y=225)
 
 # widget allows user to input their text
-user_text = tk.Text (root, wrap=WORD)
+user_text = tk.Text(root, wrap=WORD)
 user_text.config(font="calibri 13", background="white", relief=GROOVE, bd=0)
 user_text.place(x=24, y=50, width=355, height=100)
 
@@ -138,6 +146,6 @@ Button(text="RESET", height=2, width=50, bg="#1089ff", fg="white", bd=0, command
 # displays a copy to clipboard button
 # 1. button calls copy function on click
 
-### TODO
+# TODO
 
 root.mainloop()
